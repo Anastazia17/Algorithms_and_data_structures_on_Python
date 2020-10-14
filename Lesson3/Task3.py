7c16e91cc9b04dde91740382adb3772727529d98
Anastasiya <Tsybusova>
2020-10-14 14:41:29 +0300

    Done Task3

--- Lesson3/Task3.py
+++ Lesson3/Task3.py
@@ -15,4 +15,20 @@
 а
 """
 
-#Прошу досдать позже.
+import hashlib
+
+s = input("Введите строку из маленьких латинских букв: ")
+r = set()
+
+n = len(s)
+for i in range(n):
+    if i == 0:
+        n = len(s) - 1
+    else:
+        n = len(s)
+    for j in range(n, i, -1):
+        print(s[i:j])
+        r.add(hashlib.sha1(s[i:j].encode('utf-8')).hexdigest())
+
+print(r)
+print("Количество различных подстрок в строке '%s' равно %d." % (s, len(r)))
