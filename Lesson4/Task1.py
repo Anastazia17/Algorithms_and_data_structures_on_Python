033881aee2331d34234b908e95c9acfef3643bca
Anastasiya <Tsybusova>
2020-10-15 14:40:05 +0300

    Done Task1

--- Lesson4/Task1.py
+++ Lesson4/Task1.py
@@ -12,10 +12,28 @@
 Добавьте аналитику: что вы сделали и почему
 """
 
+from timeit import timeit
 
-def func_1(nums):
+print(timeit("""def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+"""))
+#Время выполнения составило: 0.050038799999999994 мс
+
+print(timeit("""
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr[len(new_arr)] = [i]
+    return new_arr
+"""))
+# Время выполнения составило: 0.0487888 мс
+
+# В данном коде вместо new_arr.append(i) используется new_arr[len(new_arr)] = [i],
+# что немного сокращает время выполнения функции.
+
+# Вывод: 2-й вариант хоть немного, но все же лучше.
