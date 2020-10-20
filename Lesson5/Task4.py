3b03c474a59fb808db5b2a1c50f32c44ff3f184d
Anastasiya <Tsybusova>
2020-10-20 20:11:33 +0300

    Done Task4

--- Lesson5/Task4.py
+++ Lesson5/Task4.py
@@ -15,7 +15,7 @@ print(timeit("""
 NEW_DICT = {'a': 1, 'b': 2, 'c': 3}
 print(NEW_DICT)
 """))
-# Время выполнения: 5.9047225 мс.
+# Время выполнения: 5.1771441 мс.
 # Почему-то словарь выводится больше 100 раз. Не понимаю почему.
 
 
@@ -25,15 +25,8 @@ print(NEW_DICT)
 print(timeit("""
 NEW_DICT = collections.OrderedDict([('a', 1), ('b', 2), ('c', 3)])
 print(NEW_DICT)
-"""))
-# Здесь измерить время не получилось из-за ошибки:
-# File "C:/Users/Vol4itsa/PycharmProjects/Algorithms_and_data_structures_on_Python/Lesson5/Task4.py", line 24, in <module>
-#    print(timeit("""
-#  File "C:\Python38\lib\timeit.py", line 233, in timeit
-#    return Timer(stmt, setup, timer, globals).timeit(number)
-#  File "C:\Python38\lib\timeit.py", line 177, in timeit
-#    timing = self.inner(it, self.timer)
-#  File "<timeit-src>", line 7, in inner
-# NameError: name 'collections' is not defined
+""", "import collections"))
+# Время выполнения: 6.9047955000000005 мс.
 
-# Вывод от себя: по идее OrderedDict должен раотать быстрее, чем обычный словарь.
+# Вывод от себя: по идее OrderedDict должен раотать быстрее, чем обычный словарь, но
+# в моем случае он работал немного дольше обычного словаря.
